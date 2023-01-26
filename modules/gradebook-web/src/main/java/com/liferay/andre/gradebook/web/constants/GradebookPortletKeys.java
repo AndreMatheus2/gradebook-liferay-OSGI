4c1174fe4e93cc3d41caffcb2e5d73807a909969
AndreMatheus2 <am_arao@hotmail.com>
2023-01-26 14:01:24 -0300

    Criando o Gradebook Web Modulo

---
+++ modules/gradebook-web/src/main/java/com/liferay/andre/gradebook/web/constants/GradebookPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.andre.gradebook.web.constants;
+
+/**
+ * @author andre-arao
+ */
+public class GradebookPortletKeys {
+
+	public static final String GRADEBOOK =
+		"com_liferay_andre_gradebook_web_portlet_GradebookPortlet";
+
+}
